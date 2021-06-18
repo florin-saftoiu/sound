2daf80b3879dab8ae969136b40f09805ea3871db
Florin Saftoiu <florin.saftoiu@soft-to-you.com>
2021-06-18 17:00:48 +0300

    draw and handle piano keyboard

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod bindings {
 }
 
 use std::f64::consts::PI;
+use std::io::{Write, stdout};
 use std::sync::{Arc, Mutex};
 use noise_maker::*;
 use bindings::Windows::{
@@ -16,7 +17,18 @@ fn main() -> windows::Result<()> {
         println!("Found Output Device: {} - {}", id, name);
     }
 
+    println!();
+    println!("|   |   |   |   |   | |   |   |   |   | |   | |   |   |   |");
+    println!("|   | S |   |   | F | | G |   |   | J | | K | | L |   |   |");
+    println!("|   |___|   |   |___| |___|   |   |___| |___| |___|   |   |__");
+    println!("|     |     |     |     |     |     |     |     |     |     |");
+    println!("|  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |");
+    println!("|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|");
+    println!();
+
     let frequency_output = Arc::new(Mutex::new(440f64));
+    let octave_base_frequency = 110f64;
+    let twelveth_root_of_2 = 2f64.powf(1f64 / 12f64);
 
     {
         let frequency_output = frequency_output.clone();
@@ -29,12 +41,37 @@ fn main() -> windows::Result<()> {
         noise_maker(0, 44100, 1, 8, 512, make_noise);
     }
 
+    let mut current_key = -1i32;
+
     loop {
+        let mut key_pressed = false;
+        for k in 0..16 {
+            if unsafe { GetAsyncKeyState(b"ZSXCFVGBNJMK\xbcL\xbe\xbf"[k] as i32) } as u16 & 0x8000 != 0 {
+                if current_key != k as i32 {
+                    let mut frequency_output  = frequency_output.lock().unwrap();
+                    *frequency_output = octave_base_frequency * twelveth_root_of_2.powi(k as i32);
+                    print!("\rNote On : {:.2}Hz", *frequency_output);
+                    let _ = stdout().flush();
+                    current_key = k as i32;
+                }
+
+                key_pressed = true;
+            }
+        }
+
+        if !key_pressed {
+            if current_key != -1 {
+                print!("\rNote Off          ");
+                let _ = stdout().flush();
+                current_key = -1;
+            }
+
+            let mut frequency_output  = frequency_output.lock().unwrap();
+            *frequency_output = 0f64;
+        }
+
         if unsafe { GetAsyncKeyState(VirtualKey::Escape.0) } as u16 & 0x8000 != 0 {
             break;
-        } else if unsafe { GetAsyncKeyState(VirtualKey::A.0) } as u16 & 0x8000 != 0 {
-            let mut frequency_output = frequency_output.lock().unwrap();
-            *frequency_output = 220f64;
         }
     }
 
